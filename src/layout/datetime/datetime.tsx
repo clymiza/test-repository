3840196d77b632a6604f9de7996c40d879a5add3
clymiza <189240736+clymiza@users.noreply.github.com>
2024-11-28 22:04:25 +0900

    feat: layout - datetime

---
+++ src/layout/datetime/datetime.tsx
@@ -0,0 +1,7 @@
+export const Datetime = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className="flex gap-2 items-center self-stretch flex-nowrap">
+			{children}
+		</div>
+	);
+};
